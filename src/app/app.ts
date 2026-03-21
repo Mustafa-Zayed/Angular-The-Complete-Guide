10e24bc1fae68d423eba565a2bdffb46c913e53f
Mustafa-Zayed <mostafa_31070285@ics.tanta.edu.eg>
2026-03-21 11:21:25 +0200

    Until 32. TypeScript Type Aliases & Interfaces

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,22 @@
-import { Component, signal } from '@angular/core';
+import { Component, EventEmitter, Output, signal } from '@angular/core';
 import { Header } from './componnets/header/header';
 import { User } from './componnets/user/user';
 import { DUMMY_USERS } from './componnets/user/dummy-users';
+import { Task } from './componnets/task/task';
 
 @Component({
   selector: 'app-root', // Component directive
-  imports: [Header, User],
+  imports: [Header, User, Task],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('first-app');
   dummyUsers = DUMMY_USERS;
+  userName? : string;
+
+  onUserSelected(userId: string) {
+    const userName = this.dummyUsers.find(user => user.id === userId)?.name; // This will find the user with the matching id and return their name. The ?. is the optional chaining operator, which will return undefined if no user is found with the matching id instead of throwing an error.
+    this.userName = userName;
+  }
 }
--- src/app/componnets/user/user.ts
+++ src/app/componnets/user/user.ts
@@ -1,7 +1,20 @@
-import { Component, computed, input, Input, signal } from '@angular/core';
+import { Component, computed, EventEmitter, input, Input, output, Output, signal } from '@angular/core';
 import { DUMMY_USERS } from './dummy-users';
 
 // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+// type UserInput = {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// };
+
+interface UserInput {
+  id: string;
+  avatar: string;
+  name: string;
+}
+
 @Component({
   selector: 'app-user',
   imports: [],
@@ -12,13 +25,22 @@ export class User {
   // // selectedUser = DUMMY_USERS[randomIndex]; // Called a state. If changed, has to be reflected in the UI.
   // selectedUser = signal(DUMMY_USERS[randomIndex]);
 
+  // @Input({ required: true }) id!: string;
   // @Input({ required: true }) avatar!: string;
   // @Input({ required: true }) name!: string;
+  // @Output() select = new EventEmitter<string>();
 
-  avatar = input.required<string>();
-  name = input.required<string>();
+  // id = input.required<string>();
+  // avatar = input.required<string>();
+  // name = input.required<string>();
 
-  imagePath = computed(() => `assets/users/${this.avatar()}`);
+  user = input.required<UserInput>();
+
+  select = output<string>(); // same as new EventEmitter<string>(). Just a more concise way to define an output event emitter and if you don't need any decorators.
+  
+  imagePath = computed(() => `assets/users/${this.user().avatar}`);
+
+  // imagePath = computed(() => `assets/users/${this.avatar()}`);
 
   // get imagePath() {
   //   return `assets/users/${this.avatar}`;
@@ -30,5 +52,9 @@ export class User {
     // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
     // // this.selectedUser = DUMMY_USERS[randomIndex]; // This will update the state and the UI will reflect the change due to zone.js detecting the change and triggering a re-render of the component.
     // this.selectedUser.set(DUMMY_USERS[randomIndex]);
+
+    // this.select.emit(this.id());
+
+    this.select.emit(this.user().id);
   }
 }
