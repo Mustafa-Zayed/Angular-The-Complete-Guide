80d2cf0290d79e13b9686deb906bbf731d579ebf
Mustafa-Zayed <mostafa_31070285@ics.tanta.edu.eg>
2026-03-26 01:34:35 +0200

    End of Section 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { SharedModule } from './componnets/shared/shared.module';
+import { TasksModule } from './componnets/tasks/tasks.module';
+
 import { App } from './app';
 import { Header } from './componnets/header/header';
 import { User } from './componnets/user/user';
-import { Tasks } from './componnets/tasks/tasks';
-import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
-  declarations: [App],
-  imports: [BrowserModule, Header, User, Tasks], // we can import the standalone components that app component needs in imports.
+  declarations: [App, Header, User],
+  imports: [BrowserModule, TasksModule, SharedModule], // we can import the standalone components that app component needs in imports.
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
 })
