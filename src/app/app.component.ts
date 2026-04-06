1e4eef9af1f76e46697c50c80ea62b713f9177c2
Mustafa-Zayed <mostafa_31070285@ics.tanta.edu.eg>
2026-04-06 20:21:33 +0200

    Until 4. Working with RxJS Operators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, interval, map, Subscription, take } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  private subscription: Subscription = new Subscription();
+
+  numbers = interval(1000);
+  takeFourNumbers = this.numbers.pipe(take(4));
+  mapNumbers = this.numbers.pipe(map((x) => x * 2)).pipe(take(4));
+
+  ngOnInit(): void {
+    this.subscription = interval(1000).subscribe(
+      // {
+      //   next: (value) => console.log(`Interval value: ${value}`),
+      //   error: (err) => console.error('Error in interval observable:', err),
+      //   complete: () => console.log('Interval observable completed.'),
+      // }
+      (value) => console.log(`Interval value: ${value}`),
+    );
+
+    setTimeout(() => {
+      if (this.subscription) {
+        this.subscription.unsubscribe();
+        console.log('Subscription unsubscribed after timeout.');
+      }
+    }, 5000);
+
+    this.takeFourNumbers.subscribe((x) => console.log('takeFourNumbers: ', x));
+    this.mapNumbers.subscribe((x) => console.log('mapNumbers: ', x));
+  }
+}
