79ba4463aff9076f7d88150e21b6b8867c65a913
Mustafa-Zayed <mostafa_31070285@ics.tanta.edu.eg>
2026-04-06 10:52:17 +0200

    Until 12. Triggering Change Detection Manually & Using RxJS Subjects

--- src/app/messages/messages-list/messages-list.component.ts
+++ src/app/messages/messages-list/messages-list.component.ts
@@ -1,4 +1,11 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  DestroyRef,
+  inject,
+  OnInit,
+} from '@angular/core';
 import { MessagesService } from '../messages.service';
 
 @Component({
@@ -8,9 +15,23 @@ import { MessagesService } from '../messages.service';
   styleUrl: './messages-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MessagesListComponent {
+export class MessagesListComponent implements OnInit {
   private messageService = inject(MessagesService);
-  messages = this.messageService.allMessages;
+  private cdRef = inject(ChangeDetectorRef);
+  private desRef = inject(DestroyRef);
+
+  messages: string[] = [];
+
+  ngOnInit(): void {
+    // Subscribe to the messages$ observable to get updates when new messages are added, so we make changes to the component's state (the "messages" property).
+    // Then mark the component for check to trigger change detection.
+    const subscription = this.messageService.messages$.subscribe((messages) => {
+      this.messages = messages;
+      this.cdRef.markForCheck();
+    });
+
+    this.desRef.onDestroy(() => subscription.unsubscribe());
+  }
 
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
