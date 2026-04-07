38135971e6eaf47b9e094e90a55a6bb9017831ed
Mustafa-Zayed <mostafa_31070285@ics.tanta.edu.eg>
2026-04-07 17:18:36 +0200

    End of Section 11. Working with RxJS (Observables) - Deep Dive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
 import { toObservable, toSignal } from '@angular/core/rxjs-interop';
-import { BehaviorSubject, interval, map, Subscription, take } from 'rxjs';
+import { BehaviorSubject, interval, map, Observable, Subscription, take } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -13,14 +13,33 @@ export class AppComponent implements OnInit {
   clickCount = signal(0);
   clickCount$ = toObservable(this.clickCount); // Convert signal to observable
 
-  interval$ = interval(1000);
-  intervalSignal = toSignal(this.interval$, { initialValue: 0 }); // Convert observable to signal
+  // interval$ = interval(1000);
+  // intervalSignal = toSignal(this.interval$, { initialValue: 0 }); // Convert observable to signal
 
   // private subscription: Subscription = new Subscription();
 
   // numbers = interval(1000);
   // takeFourNumbers = this.numbers.pipe(take(4));
   // mapNumbers = this.numbers.pipe(map((x) => x * 2)).pipe(take(4));
+
+  // In the observable initialization, we define the logic when to emit values and complete the
+  // observable. We also return a cleanup function that will be called when the subscription is unsubscribed, allowing us to clear the interval and log a message.
+  customInterval$ = new Observable<number>((subscriber) => {
+    let count = 0;
+    const intervalId = setInterval(() => {
+      subscriber.next(count++);
+    }, 1000);
+
+    if (count === 0) {
+      subscriber.complete();
+    }
+
+    // Cleanup function to clear the interval when unsubscribed
+    return () => {
+      clearInterval(intervalId);
+      console.log('Destroyed -> from observable cleanup function.');
+    };
+  });
 
   constructor() {
     // effect(() => {
@@ -53,6 +72,21 @@ export class AppComponent implements OnInit {
     this.desRef.onDestroy(() => {
       sub.unsubscribe();
       console.log('Subscription unsubscribed on component destroy.');
+    });
+
+    // In the subscription, we define what happens when a new value, complete, or error is emitted
+    const sub2 = this.customInterval$.subscribe((value) => {
+      console.log(`Custom interval value: ${value}`);
+    });
+
+    setTimeout(() => {
+      sub2.unsubscribe();
+      console.log('Destroyed -> from manual unsubscription after timeout.');
+    }, 5000);
+
+    this.desRef.onDestroy(() => {
+      sub2.unsubscribe();
+      console.log('Destroyed -> from automatic unsubscription on component destroy.');
     });
   }
 
