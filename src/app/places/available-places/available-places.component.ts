55f45ce8f4e3df7276c093917e91309037c38d60
Mustafa-Zayed <mostafa_31070285@ics.tanta.edu.eg>
2026-04-08 18:02:25 +0200

    Until 13. More Data Fetching & Some Code Duplication

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -1,8 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, Subscription, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -13,4 +15,52 @@ import { PlacesContainerComponent } from '../places-container/places-container.c
 })
 export class AvailablePlacesComponent {
   places = signal<Place[] | undefined>(undefined);
+  private httpClient = inject(HttpClient);
+  private subscriptions: Subscription[] = [];
+  isFetching = signal(false);
+  error = signal('');
+
+  ngOnInit() {
+    this.isFetching.set(true);
+
+    const sub = this.httpClient
+      .get<{ places: Place[] }>('http://localhost:3000/places', {
+        observe: 'response',
+      })
+      .pipe(
+        map((response) => response.body?.places),
+        catchError((error) => {
+          console.log(error);
+          return throwError(
+            () =>
+              new Error('Something went wrong fetching available places. Please try again later.'),
+          );
+        }),
+      )
+      .subscribe({
+        next: (places) => {
+          // console.log(places);
+          this.places.set(places);
+        },
+        error: (error: Error) => {
+          console.log(error);
+          this.error.set(error.message);
+        },
+        complete: () => this.isFetching.set(false),
+      });
+
+    this.subscriptions.push(sub);
+  }
+
+  onSelectPlace(selectedPlace: Place) {
+    this.httpClient
+      .put('http://localhost:3000/user-places', { placeId: selectedPlace.id })
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach((sub) => sub.unsubscribe());
+  }
 }
--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
+import { Place } from '../place.model';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, Subscription, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-user-places',
@@ -11,4 +14,44 @@ import { PlacesComponent } from '../places.component';
   imports: [PlacesContainerComponent, PlacesComponent],
 })
 export class UserPlacesComponent {
+  places = signal<Place[] | undefined>(undefined);
+  private httpClient = inject(HttpClient);
+  private subscriptions: Subscription[] = [];
+  isFetching = signal(false);
+  error = signal('');
+
+  ngOnInit() {
+    this.isFetching.set(true);
+
+    const sub = this.httpClient
+      .get<{ places: Place[] }>('http://localhost:3000/user-places', {
+        observe: 'body',
+      })
+      .pipe(
+        map((body) => body.places),
+        catchError((error) => {
+          return throwError(
+            () =>
+              new Error(
+                'Something went wrong fetching your favorite places. Please try again later.',
+              ),
+          );
+        }),
+      )
+      .subscribe({
+        next: (places) => {
+          this.places.set(places);
+        },
+        error: (error: Error) => {
+          this.error.set(error.message);
+        },
+        complete: () => this.isFetching.set(false),
+      });
+
+    this.subscriptions.push(sub);
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach((sub) => sub.unsubscribe());
+  }
 }
