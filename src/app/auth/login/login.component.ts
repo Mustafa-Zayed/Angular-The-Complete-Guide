3b0039e834232f3992fd5983a9813a4c67b75ecb
Mustafa-Zayed <mostafa_31070285@ics.tanta.edu.eg>
2026-04-10 20:49:56 +0200

    Until 6. Validating Input with Form Validation Directives

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
+  imports: [FormsModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginComponent {}
+export class LoginComponent {
+  onSubmit(formData: NgForm) {
+    if (formData.form.invalid) {
+      return;
+    }
+    const email = formData.form.value.email;
+    const password = formData.form.value.password;
+
+    console.log(formData);
+    // console.log(email, password);
+  }
+}
