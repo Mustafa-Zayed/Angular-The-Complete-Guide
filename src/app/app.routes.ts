22fa68234b71e5a5d7a2cc9c0b4a73276212fdf9
Mustafa-Zayed <mostafa_31070285@ics.tanta.edu.eg>
2026-04-14 19:56:18 +0200

    Until 18. Redirecting Users

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,16 +3,24 @@ import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
   {
     path: '', // <your-domain>/
     component: NoTaskComponent,
+    // redirectTo: 'users/u1',
+    // pathMatch: 'full',
   },
   {
     path: 'users/:userId', // <your-domain>/users/u2
     component: UserTasksComponent,
     children: [
+      {
+        path: '', // <your-domain>/users/u2
+        redirectTo: 'tasks',
+        pathMatch: 'full',
+      },
       {
         path: 'tasks', // <your-domain>/users/u3/tasks
         component: TasksComponent,
@@ -22,5 +30,9 @@ export const routes: Routes = [
         component: NewTaskComponent,
       },
     ],
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
   },
 ];
