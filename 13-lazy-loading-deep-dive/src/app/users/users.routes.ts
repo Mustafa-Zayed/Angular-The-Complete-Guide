b18ba9a39852cd56ee0be7ab48013be562711c01
Mustafa-Zayed <mostafa_31070285@ics.tanta.edu.eg>
2026-04-16 18:16:14 +0200

    Until 6. Using Lazy Loading & Routing to Lazy-load Services

--- 13-lazy-loading-deep-dive/src/app/users/users.routes.ts
+++ 13-lazy-loading-deep-dive/src/app/users/users.routes.ts
@@ -2,25 +2,32 @@ import { Routes } from '@angular/router';
 
 import { NewTaskComponent, canLeaveEditPage } from '../tasks/new-task/new-task.component';
 import { resolveUserTasks, TasksComponent } from '../tasks/tasks.component';
+import { TasksService } from '../tasks/tasks.service';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'tasks',
-    pathMatch: 'full',
-  },
-  {
-    path: 'tasks', // <your-domain>/users/<uid>/tasks
-    component: TasksComponent,
-    // loadComponent: () => import('../tasks/tasks.component').then((mod) => mod.TasksComponent),
-    runGuardsAndResolvers: 'always',
-    resolve: {
-      userTasks: resolveUserTasks, // declares a resolver in this file, not in the TasksComponent, so it also can be loaded lazily
-    },
-  },
-  {
-    path: 'tasks/new',
-    component: NewTaskComponent,
-    canDeactivate: [canLeaveEditPage],
+    providers: [TasksService],
+    children: [
+      {
+        path: '',
+        redirectTo: 'tasks',
+        pathMatch: 'full',
+      },
+      {
+        path: 'tasks', // <your-domain>/users/<uid>/tasks
+        component: TasksComponent,
+        // loadComponent: () => import('../tasks/tasks.component').then((mod) => mod.TasksComponent),
+        runGuardsAndResolvers: 'always',
+        resolve: {
+          userTasks: resolveUserTasks, // declares a resolver in this file, not in the TasksComponent, so it also can be loaded lazily
+        },
+      },
+      {
+        path: 'tasks/new',
+        component: NewTaskComponent,
+        canDeactivate: [canLeaveEditPage],
+      },
+    ],
   },
 ];
